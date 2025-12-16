62cfc38f181913f828d6e3d3bba19f00aabcbc70
Mike Morris <mike@mikemorris.net>
2025-12-16 12:56:51 -0800

    feat: export all analyzer module classes and functions

--- codemap/analyzer/__init__.py
+++ codemap/analyzer/__init__.py
@@ -2,6 +2,27 @@
 
 from __future__ import annotations
 
+from codemap.analyzer.ast_visitor import CodeMapVisitor, analyze_file
+from codemap.analyzer.graph import DependencyGraph
+from codemap.analyzer.impact import ImpactAnalyzer, ImpactReport
 from codemap.analyzer.pyan_wrapper import CallGraph, PyanAnalyzer
+from codemap.analyzer.symbols import (
+    SourceLocation,
+    Symbol,
+    SymbolKind,
+    SymbolRegistry,
+)
 
-__all__ = ["CallGraph", "PyanAnalyzer"]
+__all__ = [
+    "CallGraph",
+    "CodeMapVisitor",
+    "DependencyGraph",
+    "ImpactAnalyzer",
+    "ImpactReport",
+    "PyanAnalyzer",
+    "SourceLocation",
+    "Symbol",
+    "SymbolKind",
+    "SymbolRegistry",
+    "analyze_file",
+]
